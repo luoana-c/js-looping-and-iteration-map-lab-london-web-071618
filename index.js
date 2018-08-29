33bb0903d3f97d5882a158b1ecd07001be964c26
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-29 16:01:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-  drivers.map(function(driver) {
+  arr = drivers.map(function(driver) {
     const first = driver.split(' ')[0]
     const last = driver.split(' ')[1]
     
     return console.log({firstName: first, lastName: last} ) 
   })
+  return arr
 }
