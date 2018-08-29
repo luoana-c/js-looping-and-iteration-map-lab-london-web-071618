00af747ccee2732442b2b142e476f1ce1687944f
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-29 15:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function lowerCaseDrivers(drivers) {
   return drivers.map(driver => driver.toLowerCase())
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(driver => driver[firstName])
+}
